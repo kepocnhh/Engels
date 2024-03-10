88c135d590e8217df7fdbf36e777a7fb75c25559
Stanley Wintergreen <kepocnhh@gmail.com>
2024-03-10 21:28:24 +0300

    init app

---
+++ app/src/main/kotlin/org/kepocnhh/engels/MainActivity.kt
@@ -0,0 +1,24 @@
+package org.kepocnhh.engels
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.ui.platform.ComposeView
+import org.kepocnhh.engels.util.compose.BackHandler
+
+internal class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val view = ComposeView(this)
+        setContentView(view)
+        view.setContent {
+            App.Theme.Composition(
+                onBackPressedDispatcher = onBackPressedDispatcher,
+            ) {
+                BackHandler {
+                    finish()
+                }
+                // todo
+            }
+        }
+    }
+}
